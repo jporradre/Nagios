28045312d6754dd788d82efb6a29b29320f946cd
jporradre <jporradre@gmail.com>
2015-10-27 18:22:00 -0200

    Update nagios_stby_chk.sh

--- nagios_stby_chk.sh
+++ nagios_stby_chk.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+: '
+Autor:Juan Pablo Orradre
+Fecha ult. modificacion: 21/10/2015
+Descripcion: BASH script ejecutado en el server de la 
+base standby para monitorear la aplicacion de logs. 
+Llama a nagios_stby_chk.sql, quien corre la consulta de gap.
+'
 export ORACLE_SID=SID
 export ORACLE_HOME=/oracle/home
 export PATH=$ORACLE_HOME/bin:$PATH
