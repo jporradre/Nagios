75af752268872166ff14fc7e0482c0af5d362e35
jporradre <jporradre@gmail.com>
2015-11-04 11:58:25 -0300

    Update check_loadavg_linux.py
    Se le agregó revisión automática de cantidad de cores del server.

--- check_loadavg_linux.py
+++ check_loadavg_linux.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 '''
 Autor:Juan Pablo Orradre
-Fecha ult. modificacion: 27/10/2015
+Fecha ult. modificacion: 04/11/2015
 Descripcion: Plugin para Nagios para monitorear el load average de un host Linux.
 '''
 
@@ -17,8 +17,8 @@ def ayuda():
 def procesar(pHost,pServerUser):
 
     strres = "";
-
-    cmd = 'top -n 1 -b | head -n 1 | sed "s/^.*load average: //g" | tr -s "," " "';
+	
+    cmd = 'top -n 1 -b | head -n 1 | sed "s/^.*load average: //g" | tr -s "," " " ; cat /proc/cpuinfo | grep processor | wc -l';
 
     client = paramiko.SSHClient();
     key = paramiko.RSAKey.from_private_key_file("/home/nagios/.ssh/id_rsa");
@@ -35,16 +35,17 @@ def procesar(pHost,pServerUser):
         print("[ERROR] Se encontro un error en la peticion: "+res);
         exit(1);
 
-    loads = res.split(" ");
 
-    u1m = "u1min="+loads[0].strip("\n");
-    u5m = "u5min="+loads[1].strip("\n");
-    u15m = "u15min="+loads[2].strip("\n");
+    loads = res.split("\n")[0].split(" ");
+    cantCores = int(res.split("\n")[1].strip());
+	
+    u1m = "u1min="+loads[0];
+    u5m = "u5min="+loads[1];
+    u15m = "u15min="+loads[2];
 
-    strres += (u1m+' '+u5m+' '+u15m+' | '+u1m+";;;; "+u5m+";;;; "+u15m+";;;; ");
+    strres += (u1m+' '+u5m+' '+u15m+' | '+u1m+";;;; "+u5m+";"+str(cantCores)+";"+str(cantCores+(cantCores/2))+";; "+u15m+";"+str(cantCores)+";"+str(cantCores+(cantCores/2))+";; ");
     
     print(strres);
-
 
 
 if len(sys.argv) < 3:
